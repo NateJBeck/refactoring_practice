8089c6daadf2b5f85e887166189cb75e7530599d
Nathan Beck <nathan.beck10@gmail.com>
2014-10-30 13:36:53 -0400

    Add total rooms for hotels

--- hotels/hotel.rb
+++ hotels/hotel.rb
@@ -11,7 +11,20 @@ class Hotel
   def print_information
     puts "City: #{@info[:city]}"
     puts "Phone Number: #{@info[:phone]}"
-    puts "Singles: #{@info[:singles]}"
-    puts "Doubles: #{@info[:doubles]}"
+    puts "Total Rooms: #{total_rooms}"
+  end
+
+  private
+
+  def total_rooms
+    as_integer(@info[:singles]) + as_integer(@info[:doubles])
+  end
+
+  def as_integer(value)
+    value.gsub(",", "").to_i
+  end
+
+  def as_pretty_string(value)
+    value.strip
   end
 end
