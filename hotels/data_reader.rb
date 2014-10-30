2f626b996de6cc981a6eb1de35e7d1d3d9f2c78b
Nathan Beck <nathan.beck10@gmail.com>
2014-10-30 14:20:04 -0400

    Search for prettified hotel names

--- hotels/data_reader.rb
+++ hotels/data_reader.rb
@@ -5,13 +5,17 @@ class DataReader
 
   def read_data_from(file_name)
     CSV.foreach(file_name, {headers: true}) do |row|
-      name = row["Hotel"]
+      name = as_pretty_string(row["Hotel"])
       populate_database(name, row)
     end
     @hotels
   end
 
   private
+
+  def as_pretty_string(value)
+    value.strip
+  end
 
   def populate_database(name, row)
     hotel = Hotel.new(row)
